aec4754db0f7bba5b5591e9a48a0decf98176727
mamor <mamor.dev@gmail.com>
2014-05-18 03:32:22 +0900

    OAuthの"code"取得機能を作った。本家サンプル記載の"approve"と"deny"はよくわからなかったので今は無視

--- app/routes.php
+++ app/routes.php
@@ -28,3 +28,41 @@ Route::post('login', function()
 
 	return Redirect::to($auth ? 'authorize-form' : 'login');
 });
+
+// @see https://github.com/lucadegasperi/oauth2-server-laravel#authorization-code-flow
+Route::get('authorize-form', function()
+{
+	return View::make('authorize-form');
+});
+
+Route::post('oauth/authorize', array('before' => 'check-authorization-params|auth|csrf', function()
+{
+	// get the data from the check-authorization-params filter
+	$params = Session::get('authorize-params');
+
+	// get the user id
+	$params['user_id'] = Auth::user()->id;
+
+	$code = AuthorizationServer::newAuthorizeRequest('user', $params['user_id'], $params);
+
+	Session::forget('authorize-params');
+
+	return Redirect::to(AuthorizationServer::makeRedirectWithCode($code, $params));
+
+//	// check if the user approved or denied the authorization request
+//	if (Input::get('approve') !== null) {
+//
+//		$code = AuthorizationServer::newAuthorizeRequest('user', $params['user_id'], $params);
+//
+//		Session::forget('authorize-params');
+//
+//		return Redirect::to(AuthorizationServer::makeRedirectWithCode($code, $params));
+//	}
+//
+//	if (Input::get('deny') !== null) {
+//
+//		Session::forget('authorize-params');
+//
+//		return Redirect::to(AuthorizationServer::makeRedirectWithError($params));
+//	}
+}));
